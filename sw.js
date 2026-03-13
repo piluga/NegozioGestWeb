8c682ecbbb466b4b3de78ebaa16433c1d38b5383
piluga <133677361+piluga@users.noreply.github.com>
2026-03-13 11:37:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'cassa-pwa-v15';
+﻿const CACHE_NAME = 'cassa-pwa-v16';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -58,6 +58,7 @@ self.addEventListener('fetch', (event) => {
     );
 
 });
+
 
 
 
